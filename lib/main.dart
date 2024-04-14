9990247e16f02ea4fb1547f500c27a18039e08f8
abdulsatertn <abdulsatertn246@gmail.com>
2024-04-14 10:34:35 -0700

    Add QuizApp and StartScreen implementation
    Added the implementation for QuizApp and StartScreen classes with necessary
    widgets and styling. Also included the quiz logo image as an asset.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:quiz_app/start_screen.dart';
 
+void main() {
+  runApp(const QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color.fromARGB(255, 34, 12, 73),
+                    Color.fromARGB(255, 81, 37, 153),
+                  ]),
+            ),
+            child: const StartScreen()),
+      ),
+    );
+  }
+}
