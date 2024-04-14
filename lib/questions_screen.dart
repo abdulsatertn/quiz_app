bb994ea39e7f0e5a9ed4688b3bba5132992c23a9
abdulsatertn <abdulsatertn246@gmail.com>
2024-04-14 12:39:51 -0700

    Add initial question data to QuestionsScreen
    Initialize QuestionsScreen with the first question data from the questions
    list. Update AnswerButton widgets to display corresponding answer text from
    the current question.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,29 +12,35 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currintQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question ...',
-            style: TextStyle(color: Colors.white),
+          Text(
+            currintQuestion.text,
+            style: const TextStyle(color: Colors.white),
           ),
           const SizedBox(
             height: 30,
           ),
           AnswerButton(
             onTap: () {},
-            answerText: '',
+            answerText: currintQuestion.answers[0],
           ),
           AnswerButton(
             onTap: () {},
-            answerText: '',
+            answerText: currintQuestion.answers[1],
           ),
           AnswerButton(
             onTap: () {},
-            answerText: '',
+            answerText: currintQuestion.answers[2],
+          ),
+          AnswerButton(
+            onTap: () {},
+            answerText: currintQuestion.answers[3],
           ),
         ],
       ),
