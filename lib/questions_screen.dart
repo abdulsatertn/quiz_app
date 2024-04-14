06eb9fcfce1691a8cd323ec532e88af7a42b6651
abdulsatertn <abdulsatertn246@gmail.com>
2024-04-14 12:30:45 -0700

    Add AnswerButton widget and integrate it into QuestionsScreen
    Added a new AnswerButton widget to handle answer buttons in the quiz app.
    Integrated the AnswerButton widget into the QuestionsScreen for displaying answer options.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,6 +11,32 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The question ...',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(
+            onTap: () {},
+            answerText: '',
+          ),
+          AnswerButton(
+            onTap: () {},
+            answerText: '',
+          ),
+          AnswerButton(
+            onTap: () {},
+            answerText: '',
+          ),
+        ],
+      ),
+    );
   }
 }
